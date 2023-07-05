901f6d33447d1f007f3b9674bccf3fd6c2debaea
Sergey <90058274+psil0cibin@users.noreply.github.com>
2023-07-05 18:21:59 +0300

    Initial commit

---
+++ src/main/java/com/lukushin/simple_web_site/SimpleWebSiteApplication.java
@@ -0,0 +1,13 @@
+package com.lukushin.simple_web_site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleWebSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleWebSiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lukushin/simple_web_site/SimpleWebSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lukushin.simple_web_site;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleWebSiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
